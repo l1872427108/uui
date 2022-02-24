6354731e7b76e18f881575b27184e002cb2b7ca6
吴宇彪 <2421801990@qq.com>
2022-02-24 19:08:21 +0800

    uu-link开发

---
+++ examples/components/link.vue
@@ -0,0 +1,51 @@
+<template>
+	<div>
+		<div class="bottom">----------------------------------- 基础用法</div>
+		<uu-link type="default">默认链接</uu-link>
+		<uu-link type="success">主要链接</uu-link>
+		<uu-link type="primary">成功链接</uu-link>
+		<uu-link type="danger">警告链接</uu-link>
+		<uu-link type="warning">危险链接</uu-link>
+		<uu-link type="info">信息链接</uu-link>
+
+		<div class="bottom">----------------------------------- 下划线</div>
+		<uu-link type="success">主要链接</uu-link>
+		<uu-link :underline="false" type="primary">成功链接</uu-link>
+
+		<div class="bottom">----------------------------------- 禁用状态</div>
+		<uu-link type="default" disabled>默认链接</uu-link>
+		<uu-link type="success" disabled>主要链接</uu-link>
+		<uu-link type="primary" disabled>成功链接</uu-link>
+		<uu-link type="danger" disabled>警告链接</uu-link>
+		<uu-link type="warning" disabled>危险链接</uu-link>
+		<uu-link type="info" disabled>信息链接</uu-link>
+
+		<div class="bottom">----------------------------------- 图标</div>
+		<uu-link type="default" icon="uui-icon-edit">编辑</uu-link>
+		<uu-link type="success" >查看</uu-link>
+
+	</div>
+</template>
+
+<script>
+import uuLink from "./uu-link.vue";
+export default {
+	name: "UuUuiLink",
+	components: {
+		uuLink,
+	},
+	data() {
+		return {};
+	},
+
+	mounted() {},
+
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+.bottom {
+	margin: 40px 0;
+}
+</style>
---
+++ examples/components/uu-link.vue
@@ -0,0 +1,165 @@
+<template>
+	<a
+		class="uu-link"
+		:class="[
+			`uu-link--${type}`,
+			underline ? 'uu-link--underline' : '',
+			disabled ? 'uu-link--disabled' : '',
+		]"
+		:href="href"
+	>
+		<i v-if="icon" :class="icon"></i>
+		<span v-if="$slots.default">
+			<slot />
+		</span>
+	</a>
+</template>
+
+<script>
+export default {
+	name: "UuUuiUuLink",
+
+	props: {
+		type: {
+			type: String,
+			default: "default",
+			validator: function (value) {
+				return (
+					[
+						"default",
+						"success",
+						"primary",
+						"danger",
+						"warning",
+						"info",
+					].indexOf(value) !== -1
+				);
+			},
+		},
+		underline: {
+			type: Boolean,
+			default: true,
+		},
+		disabled: {
+			type: Boolean,
+			default: false,
+		},
+		icon: {
+			type: String,
+			default: "",
+		},
+		href: {
+			type: String,
+			default: "",
+		},
+	},
+	data() {
+		return {};
+	},
+
+	mouted() {},
+
+	methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+$normal-color: #000;
+$normal-active-color: #409eff;
+$primary-color: #409eff;
+$primary-active-color: #66b1ff;
+$info-color: #909399;
+$info-active-color: #a6a9ad;
+$success-color: #67c23a;
+$success-active-color: #85ce61;
+$warning-color: #e6a23c;
+$warning-active-color: #ebb563;
+$danger-color: #f56c6c;
+$danger-active-color: #f78989;
+
+.uu-link {
+	display: inline-block;
+	margin: 0 5px;
+	text-decoration: none;
+
+	&:hover {
+		cursor: pointer;
+	}
+}
+
+.uu-link--underline {
+	&:hover {
+		text-decoration: underline;
+	}
+}
+.uu-link--disabled {
+	//   pointer-events: none;
+	filter: alpha(opacity=50); /*IE滤镜，透明度50%*/
+	-moz-opacity: 0.5; /*Firefox私有，透明度50%*/
+	opacity: 0.5; /*其他，透明度50%*/
+	&:hover {
+		text-decoration: none;
+		cursor: not-allowed;
+	}
+}
+
+.uu-link [class*="uui-icon"] + span {
+	margin-left: 5px;
+}
+
+.uu-link--default {
+	color: $normal-color;
+	&:hover {
+		color: $normal-active-color;
+	}
+	&.uu-link--disabled {
+		color: $normal-color;
+	}
+}
+
+.uu-link--success {
+	color: $success-color;
+	&:hover {
+		color: $success-active-color;
+	}
+	&.uu-link--disabled {
+		color: $success-active-color;
+	}
+}
+.uu-link--primary {
+	color: $primary-color;
+	&:hover {
+		color: $primary-active-color;
+	}
+	&.uu-link--disabled {
+		color: $primary-active-color;
+	}
+}
+.uu-link--danger {
+	color: $danger-color;
+	&:hover {
+		color: $danger-active-color;
+	}
+	&.uu-link--disabled {
+		color: $danger-active-color;
+	}
+}
+.uu-link--warning {
+	color: $warning-color;
+	&:hover {
+		color: $warning-active-color;
+	}
+	&.uu-link--disabled {
+		color: $warning-active-color;
+	}
+}
+.uu-link--info {
+	color: $info-color;
+	&:hover {
+		color: $info-active-color;
+	}
+	&.uu-link--disabled {
+		color: $info-active-color;
+	}
+}
+</style>
