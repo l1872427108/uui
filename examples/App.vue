2462f1440a614bfac0ecade99ee7425ad75ec9f8
rck <296046734@qq.com>
2022-05-22 13:22:35 +0800

    first

--- examples/App.vue
+++ examples/App.vue
@@ -3,7 +3,22 @@
 		<!-- <uuButton></uuButton> -->
 		<!-- <uu-switch></uu-switch> -->
 		<!-- <uu-link></uu-link> -->
-		<uu-dialog></uu-dialog>
+		<!-- <uu-dialog></uu-dialog> -->
+		<uu-card class="box-card" shadow="hover" :body-style="{padding: '0px'}">
+			<div slot="header" class="clearfix">
+				<span>卡片名称</span>
+				<el-button style="float: right; padding: 3px 0" type="text">操作按钮</el-button>
+			</div>
+			<div v-for="o in 4" :key="o" class="text item">
+				{{'列表内容 ' + o }}
+			</div>
+		</uu-card>
+
+		<el-card class="box-card" :body-style="{padding: '0px'}">
+  <div v-for="o in 4" :key="o" class="text item">
+    {{'列表内容 ' + o }}
+  </div>
+</el-card>
   </div>
 </template>
 
@@ -13,13 +28,15 @@
 // import uuLink from './components/link.vue';
 // import uuDialog from './components/uu-dialog.vue';
 import uuDialog from './components/dialog.vue';
+import uuCard from './components/uu-card.vue';
 export default {
 	name: 'App',
 	components: {
 		// uuButton,
 		// uuSwitch,
 		// uuLink,
-		uuDialog
+		// uuDialog
+		uuCard
 	},
 	data() {
 		return {
@@ -33,5 +50,30 @@ export default {
 </script>
 
 <style>
+/* body {
+	background-color: #ccc;
+} */
 
+  .item {
+    margin-bottom: 18px;
+  }
+
+  .clearfix:before,
+  .clearfix:after {
+    display: table;
+    content: "";
+  }
+  .clearfix:after {
+    clear: both
+  }
+.box-card {
+	width: 480px;
+}
+.text {
+    font-size: 14px;
+  }
+
+  .item {
+    padding: 18px 0;
+  }
 </style>
--- examples/main.js
+++ examples/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 
 import './assets/font/iconfont.css';
 Vue.config.productionTip = false
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+Vue.use(ElementUI, {
+  size: 'small'
+})
 
 new Vue({
   render: h => h(App)
